e087dd9b6d4dd00f37cbb6e9778abb753a4edcf5
FebricoJ <riko967gmail.com>
2023-11-21 00:31:52 +0700

    project DONE

---
+++ Data/KawanLamaTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using KawanLamaTest.Model;
+
+namespace KawanLamaTest.Data
+{
+    public class KawanLamaTestContext : DbContext
+    {
+        public KawanLamaTestContext (DbContextOptions<KawanLamaTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<KawanLamaTest.Model.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿
+namespace KawanLamaTest.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public String NIK { get; set; }
+        public String Nama { get; set; }
+        public String Tanggal_Absen { get; set; }
+    }
+}
